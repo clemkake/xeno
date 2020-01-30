88923e0780237dfefed5d8f9edb2c2a825702210
KONE CLEMENT <clemkake>
2020-01-30 14:28:25 +0530

    template design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { AdminComponent } from './admin/admin.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { MenuComponent } from './employee/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    LoginComponent,
+    AdminComponent,
+    EmployeeComponent,
+    MenuComponent
+    ],
   imports: [
     BrowserModule
   ],
