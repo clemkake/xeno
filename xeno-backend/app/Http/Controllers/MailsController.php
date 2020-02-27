bfa677c2b15c26bb8091cdff66c1554dc6f3642b
Clement <elitedoh@gmail.com>
2020-02-27 19:15:20 +0530

    add the rest

---
+++ xeno-backend/app/Http/Controllers/MailsController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\mails;
+use Illuminate\Http\Request;
+
+class MailsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\mails  $mails
+     * @return \Illuminate\Http\Response
+     */
+    public function show(mails $mails)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\mails  $mails
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(mails $mails)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\mails  $mails
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, mails $mails)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\mails  $mails
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(mails $mails)
+    {
+        //
+    }
+}
---
+++ xeno-backend/app/Http/Controllers/ReportsController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\reports;
+use Illuminate\Http\Request;
+
+class ReportsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\reports  $reports
+     * @return \Illuminate\Http\Response
+     */
+    public function show(reports $reports)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\reports  $reports
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(reports $reports)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\reports  $reports
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, reports $reports)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\reports  $reports
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(reports $reports)
+    {
+        //
+    }
+}
